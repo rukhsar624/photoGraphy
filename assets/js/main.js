0a5905cb18ee466fd114ca2302ea0fecbbf34308
rukhsar624 <rukhsaranwar56@gmail.com>
2023-03-22 17:55:18 +0500

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,69 @@
+$(window).on('load',function() {
+
+	$(".gallerSec section img").click(function() {
+	  $(".gallerSec .lightbox").fadeIn(300);
+	  $(".gallerSec .lightbox").append("<img src='" + $(this).attr("src") + "' alt='" + $(this).attr("alt") + "' />");
+	  $(".gallerSec .filter").css("background-image", "url(" + $(this).attr("src") + ")");
+	  /*$(".title").append("<h1>" + $(this).attr("alt") + "</h1>");*/
+	  $("html").css("overflow", "hidden");
+	  if ($(this).is(":last-child")) {
+		$(".gallerSec .arrowr").css("display", "none");
+		$(".gallerSec .arrowl").css("display", "block");
+	  } else if ($(this).is(":first-child")) {
+		$(".gallerSec .arrowr").css("display", "block");
+		$(".gallerSec .arrowl").css("display", "none");
+	  } else {
+		$(".gallerSec .arrowr").css("display", "block");
+		$(".gallerSec .arrowl").css("display", "block");
+	  }
+	});
+  
+	$(".gallerSec .close").click(function() {
+	  $(".gallerSec .lightbox").fadeOut(300);
+	  $(".gallerSec h1").remove();
+	  $(".gallerSec .lightbox img").remove();
+	  $("html").css("overflow", "auto");
+	});
+  
+	$(document).keyup(function(e) {
+	  if (e.keyCode == 27) {
+		$(".gallerSec .lightbox").fadeOut(300);
+		$(".gallerSec .lightbox img").remove();
+		$("html").css("overflow", "auto");
+	  }
+	});
+  
+	$(".gallerSec .arrowr").click(function() {
+	  var imgSrc = $(".gallerSec .lightbox img").attr("src");
+	  var search = $(".gallerSec section").find("img[src$='" + imgSrc + "']");
+	  var newImage = search.next().attr("src");
+	  /*$(".lightbox img").attr("src", search.next());*/
+	  $(".gallerSec .lightbox img").attr("src", newImage);
+	  $(".gallerSec .filter").css("background-image", "url(" + newImage + ")");
+  
+	  if (!search.next().is(":last-child")) {
+		$(".gallerSec .arrowl").css("display", "block");
+	  } else {
+		$(".gallerSec .arrowr").css("display", "none");
+	  }
+	});
+  
+	$(".gallerSec .arrowl").click(function() {
+	  var imgSrc = $(".gallerSec .lightbox img").attr("src");
+	  var search = $(".gallerSec section").find("img[src$='" + imgSrc + "']");
+	  var newImage = search.prev().attr("src");
+	  /*$(".lightbox img").attr("src", search.next());*/
+	  $(".gallerSec .lightbox img").attr("src", newImage);
+	  $(".gallerSec .filter").css("background-image", "url(" + newImage + ")");
+  
+	  if (!search.prev().is(":first-child")) {
+		$(".gallerSec .arrowr").css("display", "block");
+	  } else {
+		$(".gallerSec .arrowl").css("display", "none");
+	  }
+	});
+  
+  });
 // scroll navbar
 $(window).scroll(function () {
 	var scroll = $(window).scrollTop();
@@ -41,3 +107,4 @@ window.addEventListener('keydown', function (event) {
 		sidebarBox.classList.remove('active');
 	}
 });
+// image gallery
